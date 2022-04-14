14ff83dbaafd54244a3f429c29f4ddb6df857b25
Surawit Yosaeng <surawityosaeng@192.168.0.107>
2022-04-15 00:06:45 +0700

    add exchange list

--- src/components/WebSelectBar.tsx
+++ src/components/WebSelectBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Typography,
   Paper,
@@ -7,48 +7,63 @@ import {
   FormControl,
   FormGroup,
   FormControlLabel,
-} from '@mui/material';
+} from "@mui/material";
 
 interface IWebSelectBar {}
 
+const exchangeList = [
+  "Binance",
+  "Bitfinex",
+  "Coinbase",
+  "Coinsbit",
+  "eToroX",
+  "FTX",
+  "Gateio",
+  "Kucoin",
+];
+
 const WebSelectBar: React.FC<IWebSelectBar> = ({}: IWebSelectBar) => {
   const websites = [
-    { website: 'Explore Price', route: '/search' },
-    { website: 'About', route: '/about' },
-    { website: 'Nav3', route: '/' },
+    { website: "Explore Price", route: "/search" },
+    { website: "About", route: "/about" },
+    { website: "Nav3", route: "/" },
   ];
   return (
     <Paper
       sx={{
-        borderStyle: 'solid',
+        borderStyle: "solid",
         m: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <Typography sx={{ textAlign: 'center' }}>Exchange Websites</Typography>
+      <Typography sx={{ textAlign: "center" }}>Exchange Websites</Typography>
       <FormGroup
         sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          borderStyle: 'solid',
-          justifyContent: 'center',
+          display: "flex",
+          flexWrap: "wrap",
+          borderStyle: "solid",
+          justifyContent: "center",
           width: 0.7,
         }}
       >
-        <FormControlLabel
-          control={<Checkbox />}
-          label='label'
-          sx={{
-            flexGrow: 1,
-            maxWidth: 0.21,
-            width: 0.21,
-            borderStyle: 'solid',
-            maxHeight: 40,
-            m: 2,
-          }}
-        />
+        {exchangeList.map((data) => {
+          return (
+            <FormControlLabel
+              control={<Checkbox />}
+              label={data}
+              sx={{
+                flexGrow: 1,
+                maxWidth: 0.21,
+                width: 0.21,
+                borderStyle: "solid",
+                maxHeight: 40,
+                m: 2,
+              }}
+            />
+          );
+        })}
       </FormGroup>
     </Paper>
   );
