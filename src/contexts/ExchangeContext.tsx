63a5b0a3135c2f7ae09362542e113cc6dfe32f7b
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2022-04-28 17:26:14 +0700

    Add formatter

--- src/contexts/ExchangeContext.tsx
+++ src/contexts/ExchangeContext.tsx
@@ -10,67 +10,89 @@
 import React, { createContext, useState } from "react";
 import { ExchangeContextType, IExchange } from "../@types/exchange";
 import exchangeService from "../services/exchange.service";
+import {
+  AbstractFormatter,
+  BinanceFormatter,
+  BitMartFormatter,
+  GateIoFormatter,
+  KuCoinFormatter,
+} from "../services/formatter.service";
 
 export const ExchangeContext = createContext<ExchangeContextType | null>(null);
 
 const InitExchange: IExchange[] = [
-    {
-        name: "Binance",
-        isSelect: true,
-    },
-    {
-        name: "Gateio",
-        isSelect: true,
-    },
-    {
-        name: "Kucoin",
-        isSelect: true,
-    },
-    {
-        name: "BitMart",
-        isSelect: true,
-    },
+  {
+    name: "Binance",
+    isSelect: true,
+  },
+  {
+    name: "Gateio",
+    isSelect: true,
+  },
+  {
+    name: "Kucoin",
+    isSelect: true,
+  },
+  {
+    name: "BitMart",
+    isSelect: true,
+  },
 ];
 
 export const ExchangeProvider: React.FC<React.ReactNode> = ({ children }) => {
-    const [exchanges, setExchanges] = useState<IExchange[]>(InitExchange);
-    const [data, setData] = useState<any>([]);
-    /**
-     * @dev fetchPrice will fetch price data from backend
-     * and update to data state for provide to every conponent in project
-     * by React.useContext
-     * 
-     * @param name The name of cryptocurrency for fetch price data.
-     */
-    const fetchPrice = (name: string) => {
-        exchangeService.currency(name).then((res) => {
-            console.log(res.data);
-        });
-    };
+  const [exchanges, setExchanges] = useState<IExchange[]>(InitExchange);
+  const [data, setData] = useState<any>([]);
+  /**
+   * @dev fetchPrice will fetch price data from backend
+   * and update to data state for provide to every conponent in project
+   * by React.useContext
+   *
+   * @param name The name of cryptocurrency for fetch price data.
+   */
+  const fetchPrice = (name: string) => {
+    exchangeService.currency(name).then((res: any) => {
+      console.log(res.data);
 
-    const toggleSelect = (name: string, isSelect: boolean) => {
-        exchanges.filter((exchange: IExchange) => {
-            if (exchange.name === name) {
-                exchange.isSelect = !exchange.isSelect;
-                //setExchanges({ ...exchanges, [name]: false});
-                // Why Error!! I can't update exchange to exchanges
-                // setExchanges([...exchange])
-            }
-            setExchanges([...exchanges]);
-        });
-        console.log(...exchanges);
-        // dont know how tf this works... but it works
-        // check the result in console
-        // (accidentally) ; Bean
-    };
+      const biFormatter: AbstractFormatter = new BinanceFormatter(
+        res.data["bn"]
+      );
+      console.log(biFormatter.getFormattedData());
+      const bmFormatter: AbstractFormatter = new BitMartFormatter(
+        res.data["bm"]
+      );
+      console.log(bmFormatter.getFormattedData());
+      const gaFormatter: AbstractFormatter = new GateIoFormatter(res.data["g"]);
+      console.log(gaFormatter.getFormattedData());
+      const kuFormatter: AbstractFormatter = new KuCoinFormatter(
+        res.data["kc"]
+      );
+      console.log(kuFormatter.getFormattedData());
+    });
+  };
 
-    return (
-        <ExchangeContext.Provider
-            value={{ exchanges, toggleSelect, data, fetchPrice }}
-        >
-            {children}
-        </ExchangeContext.Provider>
-    );
+  const toggleSelect = (name: string, isSelect: boolean) => {
+    exchanges.filter((exchange: IExchange) => {
+      if (exchange.name === name) {
+        exchange.isSelect = !exchange.isSelect;
+        //setExchanges({ ...exchanges, [name]: false});
+        // Why Error!! I can't update exchange to exchanges
+        // setExchanges([...exchange])
+      }
+      setExchanges([...exchanges]);
+    });
+    console.log(...exchanges);
+    // dont know how tf this works... but it works
+    // check the result in console
+    // (accidentally) ; Bean
+  };
+
+  return (
+    <ExchangeContext.Provider
+      value={{ exchanges, toggleSelect, data, fetchPrice }}
+    >
+      {children}
+    </ExchangeContext.Provider>
+  );
 };
 
 export default ExchangeProvider;
