2e36256250d1d88ddacdf65a6d400341609c9c42
teethat-lsk <63555111+teethat-lsk@users.noreply.github.com>
2022-04-28 22:39:20 +0700

    mod card

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -25,7 +25,7 @@ const SearchPage: React.FC<SearchPageProps> = ({}: SearchPageProps) => {
     <div className="flex flex-col items-center">
       <SearchBox />
       <WebSelectBar />
-      <Paper className='flex flex-wrap justify-center'>
+      <Paper className='flex flex-wrap justify-center w-full'>
         {exchanges.map(({data,isSelect}) => {
         if(data && isSelect)
         return (
